319522f0f1838623bfdf865976f1e526a6164a1f
Yash Deshpande <yvd.yash@gmail.com>
2018-06-29 22:00:01 +0530

    Add files via upload

--- src/test_trained_webcam.py
+++ src/test_trained_webcam.py
@@ -7,6 +7,8 @@ from PIL import Image, ImageDraw
 import face_recognition
 from face_recognition.face_recognition_cli import image_files_in_folder
 import cv2
+#from webapp import db
+#from DB import Actor
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
@@ -43,12 +45,18 @@ def predict(X_img, knn_clf=None, model_path=None, distance_threshold=0.6):
     return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
 
 
-if __name__ == "__main__":
-    
-    video_capture = cv2.VideoCapture(0)
 
+
+def faceRec(vid_path):
+    video_capture = cv2.VideoCapture(vid_path)
+    length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_no=0
+    print(vid_path)
+    print("Number of total frames are: {}".format(length))
     while True:
         ret, frame = video_capture.read()
+        if not ret:
+            break
         rgb_frame = frame[:, :, ::-1]
         predictions=predict(rgb_frame,model_path="models/trained_knn_model.clf")
         for name, (top, right, bottom, left) in predictions:
@@ -64,7 +72,16 @@ if __name__ == "__main__":
         # Hit 'q' on the keyboard to quit!
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        frame_no+=1
+        print("Writing frame {} / {}".format(frame_no, length))
+        # if predictions:
+        #     actor=Actor(actorname=predictions[0],frameno=frame_no,image_file="default.jpg",x=predictions[1][0],y=predictions[1][1],z=predictions[1][2],w=predictions[1][3])
+        #     db.session.add(actor)
+        #     db.session.commit()
 
     # Release handle to the webcam
     video_capture.release()
     cv2.destroyAllWindows()
+# destination="/home/yash/Desktop/face_recognition-master/examples/short_hamilton_clip.mp4"
+destination="/home/yash/Desktop/Final/Face reco KNN/src/short_hamilton_clip.mp4"
+faceRec(destination)
---
+++ src/webapp.py
@@ -0,0 +1,131 @@
+import os, sys, csv, cv2, subprocess, numpy
+from subprocess import call
+#import video_emotion_color_demo_rahul as veg
+#import speech_recognition as sr
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+#import searchresults as searchresults
+import test_trained_webcam as face_reco
+from flask_sqlalchemy import SQLAlchemy
+import unicodedata
+
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///side.db'
+app.config['UPLOAD_FOLDER']='/home/yash/Desktop/Final/Face reco KNN/src/uploadfolder'
+db=SQLAlchemy(app)
+
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+mypath = '/home/'
+videofiles = []
+csvfiles = []
+d = []
+e = []
+frame_no = []
+thumbnail_time = []
+files = os.listdir(mypath)
+sep = '.'
+
+
+@app.route("/")
+def index():
+    return render_template("/index.html")
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    for file in request.files.getlist("file"):
+        filename = secure_filename(file.filename)
+        print(filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        destination=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        # target1 = os.path.dirname(file)
+        # filename = file.filename
+        # print(file)
+        # destination = os.path.abspath(file)
+        print(destination)
+        #destination = str(destination)
+        #destination="/home/yash/Desktop/Final/Face reco KNN/src/The Speech that Made Obama President.mp4"
+        #unicodedata.normalize('NFKD', destination).encode('ascii','ignore')
+        #unicodedata.normalize('NFKD', destination).encode('ascii','ignore')
+        #print(type(destination))
+        #destination="/home/yash/Desktop/face_recognition-master/examples/short_hamilton_clip.mp4"
+        face_reco.faceRec(destination)
+
+        #veg.func(destination)
+
+    return render_template("complete.html", value = destination)
+
+# @app.route("/search", methods=['GET', 'POST'])
+# def search():
+    # for file in os.listdir(mypath):
+    #     if file.endswith(".csv"):
+    #         a = file.split(sep, 1)[0]
+    #         t = file.split('.')
+    #         #e = e + t
+    #         e.append(t[0])
+    #         e.append(t[1])
+    #         csvfiles.append(a)
+
+
+    # for file in os.listdir(mypath):
+    #     if file.endswith((".mp4", ".mkv", ".flv", ".wmv", ".avi", ".mpg", ".mpeg")):
+    #         b = file.split(sep, 1)[0]
+    #         t = file.split('.')
+        
+    #         videofiles.append(b)
+
+    # print(e)
+    # c = list(set(videofiles).intersection(set(csvfiles)))
+
+    # destination = []
+    # j=0
+    # for j in range(len(e)):
+    #     for i in range(len(c)):
+    #         if(c[i]==e[j]):
+    #             c[i]=c[i] + '.' + e[j+1]
+    #             exe=c[i]
+    #             print(os.path.abspath(c[i]))
+    #             #i = i + 1
+    #             #j = j + 2
+    #     j=j+1
+    # name = []
+    # name = request.form['Query']
+    # filepath = os.path.abspath(c[i])
+    # frame_no = searchresults.process(filepath, name)
+
+##    cap = cv2.VideoCapture(filepath)
+##    fps = cap.get(cv2.CAP_PROP_FPS)
+##    for p in range(len(frame_no)):
+##        cap.set(1, float(frame_no[p]))
+##        ret, frame = cap.read()
+##        cv2.imwrite('trial%d.jpg' % float(frame_no[p]), frame)     
+##        thumbnail_time[p] = float(frame_no[p]/fps)
+##
+##
+##    return render_template("thumbnail.html", filepathvalue = filepath, thumbnailvalue = thumbnail_time  )
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+##    cap = cv2.VideoCapture('joined-all.mp4')
+##    for p in range(len(frame_no)):
+##        vdo.vpl(fra)
+##        print("Video number "+ str(p+1) +" has ended.")
+##
+        
+##        print(" Entering the for loop")
+##        cap.set(1, float(frame_no[p]))
+##        
+##        print(" The frame with the required actor and the object is : " + frame_no[p])
+##        ret, frame = cap.read()
+##        cv2.imshow('window_name', frame)
+##        while True:
+##            ch = 0xFF & cv2.waitKey(1)
+##            if ch == 27:
+##                break 
