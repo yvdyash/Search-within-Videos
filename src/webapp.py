8947fbc13ea2a3810cf8b53b6f2a404a0f30971b
Yash Deshpande <yvd.yash@gmail.com>
2018-07-02 23:39:49 +0530

    Update webapp.py

--- src/webapp.py
+++ src/webapp.py
@@ -8,14 +8,33 @@ from werkzeug import secure_filename
 import test_trained_webcam as face_reco
 from flask_sqlalchemy import SQLAlchemy
 import unicodedata
-
+#from DB import Actor
 
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///side.db'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site1.db'
 app.config['UPLOAD_FOLDER']='/home/yash/Desktop/Final/Face reco KNN/src/uploadfolder'
 db=SQLAlchemy(app)
+
+
+
+class Actor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    actorname = db.Column(db.String(50))
+    frameno = db.Column(db.Integer)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    x = db.Column(db.Integer)
+    y = db.Column(db.Integer)
+    z = db.Column(db.Integer)
+    w = db.Column(db.Integer)
+
+    def __repr__(self):
+        #return "Actor('{self.actorname}', '{self.frameno}', '{self.image_file}','{self.x}','{self.y}','{self.z}','{self.w}')"
+        return "Actor('{}', '{}', '{}','{}','{}','{}','{}')".format(self.actorname,self.frameno,self.image_file,self.x,self.y,self.z,self.w)
+
+db.create_all()
+
 
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -45,9 +64,49 @@ def upload():
         destination=os.path.join(app.config['UPLOAD_FOLDER'], filename)
         print(destination)
         face_reco.faceRec(destination)
-
-        
+  
     return render_template("complete.html", value = destination)
+
+
+actor_dict={}
+
+@app.route("/search", methods=['GET', 'POST'])
+def search():
+
+    Query=request.form['query']
+    Query=Query.split(" ")
+    Actorss=Actor.query.all()
+    print(Actorss)
+    for act in Actorss:
+        if act.actorname in actor_dict.keys():
+            actor_dict[act.actorname].append(act.frameno)
+        else:
+            actor_dict[act.actorname]=[]
+            actor_dict[act.actorname].append(act.frameno)
+
+    print(actor_dict)
+    #Query=['salmankhan','amitabhbachchan']
+    for i in actor_dict.keys():
+        actor_dict[i]=set(actor_dict[i])
+    print(actor_dict[Query[0]].intersection(actor_dict[Query[1]]))
+
+    return render_template("complete.html")
+
+            
+            
+
+    
+# with open(win.csvname, 'rt') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         for element in row[4].split(" "):
+#             #print('Entering for loop')
+#             if row[4] in actordic.keys():
+#                 actordic[row[4]].append(row[1])
+#             else:
+#                 actordic[row[4]]=[]
+#                 actordic[row[4]].append(row[1])
+
 
 # @app.route("/search", methods=['GET', 'POST'])
 # def search():
